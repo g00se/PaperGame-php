437985a0d2b6e43323f8a163b6aa8bdda6077df9
Andrew <33059451+astet-gag@users.noreply.github.com>
2020-03-28 16:57:08 +0300

    First upload

---
+++ math-coin.php
@@ -0,0 +1,58 @@
+<?php
+	/*
+	 * @author Astet (Andrew Galenko)
+	 * @version 1.0
+	 */
+	
+	class MathApi {
+		private $url = "https://mathbattle.12kot3k.ru/public_api.php";
+		private $key = '';
+		private $uid = '';
+
+		public function __construct($uid,$key){
+			$this->key = $key;
+			$this->uid = $uid;
+		}
+
+		private function request($data){
+			$data['token'] = $this->key;
+			$ch = curl_init();
+			curl_setopt_array($ch, [
+				CURLOPT_URL => $this->url,
+				CURLOPT_POST => true,
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_POSTFIELDS => json_encode($data,JSON_UNESCAPED_UNICODE)
+			]);
+			$response = curl_exec($ch);
+			$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+			curl_close($ch);
+			return json_decode($response,true);
+		}
+
+		public function score($id){
+			return $this->request([
+				'method' => 'score',
+				'id' => $id
+			])['score'];
+		}
+
+		public function send($toId,$sum){
+			return $this->request([
+				'method' => 'send_score',
+				'to' => $toId,
+				'amount' => $sum
+			]);
+		}
+
+		public function txList($ltx){
+			return $this->request([
+				'method' => 'tx_list',
+				'lastTx' => $ltx
+			])['tx_list'];
+		}
+
+		public function txLink($sum){
+			return 'https://vk.com/app6995668#t'.$this->uid.'_'.$sum;
+		}
+	}
+?>
